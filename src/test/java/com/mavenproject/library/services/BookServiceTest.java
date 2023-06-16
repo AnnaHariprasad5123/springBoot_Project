49e14a99953c1a0f926bfd01b9ff4e875ea0589e
AnnaHariprasad5123 <124746204+AnnaHariprasad5123@users.noreply.github.com>
2023-06-16 11:49:19 +0530

    Some testcases are added

--- src/test/java/com/mavenproject/library/services/BookServiceTest.java
+++ src/test/java/com/mavenproject/library/services/BookServiceTest.java
@@ -1,6 +1,10 @@
 package com.mavenproject.library.services;
 
+import com.mavenproject.library.dao.AuthorRepository;
 import com.mavenproject.library.dao.BookRepository;
+import com.mavenproject.library.dao.PublisherRepository;
+import com.mavenproject.library.entity.Author;
+import com.mavenproject.library.entity.Publisher;
 import com.mavenproject.library.service.BookService;
 import com.mavenproject.library.entity.Book;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,10 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -21,6 +22,12 @@ class BookServiceTest {
 
     @Mock
     private BookRepository bookRepository;
+
+     @Mock
+    private AuthorRepository authorRepository;
+
+    @Mock
+    private PublisherRepository publisherRepository;
 
     @InjectMocks
     private BookService bookService;
@@ -74,5 +81,49 @@ class BookServiceTest {
         bookService.deleteBook(bookId);
 
         verify(bookRepository, times(1)).deleteById(bookId);
+    }
+
+    @Test
+    void testAssignAuthorToBook() {
+        Long bookId = 1L;
+        Long authorId = 1L;
+        Book book = new Book();
+        Author author = new Author();
+        Set<Author> authorSet = new HashSet<>();
+        authorSet.add(author);
+
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(authorRepository.findById(authorId)).thenReturn(Optional.of(author));
+        when(bookRepository.save(book)).thenReturn(book);
+
+        Book result = bookService.assignAuthorToBook(bookId, authorId);
+
+        assertNotNull(result);
+        assertEquals(authorSet, result.getAuthors());
+        verify(bookRepository, times(1)).findById(bookId);
+        verify(authorRepository, times(1)).findById(authorId);
+        verify(bookRepository, times(1)).save(book);
+    }
+
+    @Test
+    void testAssignPublisherToBook() {
+        Long bookId = 1L;
+        Long publisherId = 1L;
+        Book book = new Book();
+        Publisher publisher = new Publisher();
+        Set<Publisher> publisherSet = new HashSet<>();
+        publisherSet.add(publisher);
+
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(publisherRepository.findById(publisherId)).thenReturn(Optional.of(publisher));
+        when(bookRepository.save(book)).thenReturn(book);
+
+        Book result = bookService.assignPublisherToBook(bookId, publisherId);
+
+        assertNotNull(result);
+        assertEquals(publisherSet, result.getPublishers());
+        verify(bookRepository, times(1)).findById(bookId);
+        verify(publisherRepository, times(1)).findById(publisherId);
+        verify(bookRepository, times(1)).save(book);
     }
 }
